ffa8de9b21140176fb7f9ddf0a1c98c1fbb7b2eb
quliang <985444288@qq.com>
2019-09-16 10:20:04 +0800

    添加layout组件

--- src/entry/env_index/index.js
+++ src/entry/env_index/index.js
@@ -10,23 +10,60 @@ import Vue from 'vue/dist/vue.esm.js';
 import vueRouter from 'vue-router';
 import router from 'router';
 import vuex from 'vuex';
+import {Container,Header,Aside,Main,Row,Col,Menu,Submenu,MenuItem,MenuItemGroup,Icon} from 'element-ui';
+// import 'element-u/i/lib/theme-chalk/index.css';
+import leftNav from '$views/layout/leftNav.vue';
+import headNav from '$views/layout/headNav.vue';
 
 Vue.use(vueRouter)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Icon)
 
 new Vue({
     el: '#text',
     data: {
         msg: '1111',
+        toggleCol: true
+    },
+    components: {
+        leftNav, headNav
     },
     router,
     render(h) {
         return (
             <div>
-                aaaaaa
                 <transition name="fade">
                     <router-view class="view"></router-view>
                 </transition>
+
+                {/* <el-container>
+                    <el-aside style="display:inline-block;width:auto;height:auto;">
+                        <left-nav toggle={toggleCol}></left-nav>
+                    </el-aside>
+                    <el-container>
+                        <el-header style="padding:0;">
+                            <head-nav toggleFold="toggleFold"></head-nav>
+                        </el-header>
+                        <el-main style="background:rgba(240,241,241,1);">
+                            <router-view></router-view>
+                        </el-main>
+                    </el-container>
+                </el-container> */}
             </div>
         )
+    },
+    methods:{
+        toggleFold(param) {
+            this.toggleCol = param;
+          }
     }
 })
